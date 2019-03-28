19febd630ea8ad2a4f0f41abea9f180a52827f0a
wanxiangcc <308243528@qq.com>
2019-03-28 15:44:05 +0800

    test

--- consumer-test-feign/src/main/java/com/wx/consumer/test/feign/TestController.java
+++ consumer-test-feign/src/main/java/com/wx/consumer/test/feign/TestController.java
@@ -1,6 +1,7 @@
 package com.wx.consumer.test.feign;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,16 @@ public class TestController {
     @Autowired
     GoodsManager goodsManager;
 
+    @Value("${testKey}")
+    String configName;
+
     @RequestMapping("/msg")
     public String msg() {
         return goodsManager.msg();
+    }
+
+    @RequestMapping("/config")
+    public String config() {
+        return configName;
     }
 }
